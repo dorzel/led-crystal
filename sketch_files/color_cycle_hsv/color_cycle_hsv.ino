4ddf37a6b4f090813996e098919b9ebba53df254
Dylan Orzel <dylan.orzel@gmail.com>
2020-12-19 17:52:21 -0700

    Update color_cycle_hsv.ino

--- sketch_files/color_cycle_hsv/color_cycle_hsv.ino
+++ sketch_files/color_cycle_hsv/color_cycle_hsv.ino
@@ -3,6 +3,7 @@
 const int redPin = 10;
 const int greenPin = 11;
 const int bluePin = 9;
+const int delayMS = 130;
 
 // representation of one RGB pixel
 CRGB rgbColor;
@@ -15,50 +16,22 @@ void setColorRgb(unsigned int red, unsigned int green, unsigned int blue) {
   analogWrite(bluePin, blue);
 }
 
-void fullValueCycle(bool invert, int delayMS) {
-  // run the hsv pixel through value 0-255, with constant hue and saturation
-  // can choose to invert the direction to 255-0
-  for (int i = 0; i <= 255; i += 1) {
-      if (invert) {
-        hsvColor.v = 255 - i; 
-      }
-      else {
-        hsvColor.v = i;
-      }
-      hsv2rgb_rainbow(hsvColor, rgbColor);
-      setColorRgb(rgbColor.r, rgbColor.b, rgbColor.g);
-      delay(delayMS);
-    }
-}
-
-void fullHueCycle(int delayMS) {
+void fullHueCycle() {
   // run the hsv pixel through hue 0-255, with constant value and saturation
   // no need to have an invert option as hue is defined as the angular component
   // in cylindrical coordinates, and so will return to it's originial value
   for (int i = 0; i <= 255; i++) {
     hsvColor.h = i;
     hsv2rgb_rainbow(hsvColor, rgbColor);
-    setColorRgb(rgbColor.r, rgbColor.b, rgbColor.g);
-    delay(delayMS);
-  }
-}
-
-void startupSequence(int startHue, int numTimes, int valueDelayMS) {
-  // pulsing effect with a single hue, make sure that this runs
-  // an odd number of times to end on the value being max
-  hsvColor.h = startHue;
-  for (int i = 0; i < numTimes; i++) {
-    bool invert = (i + 2) % 2;
-    fullValueCycle(invert, valueDelayMS); 
-  }
+    setColorRgb(rgbColor.r, rgbColor.g, rgbColor.b);
+    delay(delayMS); 
+  } 
 }
 
 void setup() {
-  // Start off with the LED off.
   setColorRgb(0,0,0);
-  startupSequence(0, 5, 4);
 }
 
 void loop() {
-  fullHueCycle(125);
+  fullHueCycle();
 }
